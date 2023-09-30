d825cfd12062d0e33410a9432fb7121b9d5a9f8a
Kushin-Oleg <145278779+Kushin-Oleg@users.noreply.github.com>
2023-09-30 21:17:23 +0300

    Update main.cpp

--- contest_02/01/main.cpp
+++ contest_02/01/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <cmath>
 
-// Ваш код будет вставлен сюда
+bool is_prime(int number){
+    if (number<2){return false;};
+    for (int i=2; i<=std::sqrt(number);i++){
+        if(number%i==0){return false;};
+    }
+    return true;
+}
 
 int main(){
     int number;
