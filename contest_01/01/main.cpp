f4e7bf3695eb452d9371bc05f3009b188da94d24
Kushin-Oleg <145278779+Kushin-Oleg@users.noreply.github.com>
2023-09-30 20:39:19 +0300

    Delete contest_01/01 directory

--- contest_01/01/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <math.h>
-
-int main(){
-    double a ((sqrt(12))*(1.0-(1.0/9)+(1.0/(5*3*3))-(1.0/(7*3*3*3))+(1.0/(9*3*3*3*3))-(1.0/(11*3*3*3*3*3))));
-    std::cout << a;
-    return 0;
-}
